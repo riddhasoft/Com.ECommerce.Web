52b6d99cef7c13f595ee82e3bcb4bf878f56df46
Binod sapota <binodsapkota96@gmail.com>
2023-07-04 18:08:47 +0545

    Add project files.

---
+++ Com.ECommerce.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Com.ECommerce.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Com.ECommerce.Web/Models/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Com.ECommerce.Web.Models
+{
+    public class ProductCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
