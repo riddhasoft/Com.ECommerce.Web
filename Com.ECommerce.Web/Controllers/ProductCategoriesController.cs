fa517e6926df374b0263f121c3db3fac29f6028a
Binod sapota <binodsapkota96@gmail.com>
2023-07-04 18:29:24 +0545

    added context through scafolding and migrations added

---
+++ Com.ECommerce.Web/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Com.ECommerce.Web.Data;
+using Com.ECommerce.Web.Models;
+
+namespace Com.ECommerce.Web.Controllers
+{
+    public class ProductCategoriesController : Controller
+    {
+        private readonly ECommerceDBContext _context;
+
+        public ProductCategoriesController(ECommerceDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProductCategories
+        public async Task<IActionResult> Index()
+        {
+              return _context.ProductCategory != null ? 
+                          View(await _context.ProductCategory.ToListAsync()) :
+                          Problem("Entity set 'ECommerceDBContext.ProductCategory'  is null.");
+        }
+
+        // GET: ProductCategories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.ProductCategory == null)
+            {
+                return NotFound();
+            }
+
+            var productCategory = await _context.ProductCategory
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.ProductCategory == null)
+            {
+                return NotFound();
+            }
+
+            var productCategory = await _context.ProductCategory.FindAsync(id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+            return View(productCategory);
+        }
+
+        // POST: ProductCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] ProductCategory productCategory)
+        {
+            if (id != productCategory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductCategoryExists(productCategory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.ProductCategory == null)
+            {
+                return NotFound();
+            }
+
+            var productCategory = await _context.ProductCategory
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(productCategory);
+        }
+
+        // POST: ProductCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.ProductCategory == null)
+            {
+                return Problem("Entity set 'ECommerceDBContext.ProductCategory'  is null.");
+            }
+            var productCategory = await _context.ProductCategory.FindAsync(id);
+            if (productCategory != null)
+            {
+                _context.ProductCategory.Remove(productCategory);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductCategoryExists(int id)
+        {
+          return (_context.ProductCategory?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Com.ECommerce.Web/Data/ECommerceDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Com.ECommerce.Web.Models;
+
+namespace Com.ECommerce.Web.Data
+{
+    public class ECommerceDBContext : DbContext
+    {
+        public ECommerceDBContext (DbContextOptions<ECommerceDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Com.ECommerce.Web.Models.ProductCategory> ProductCategory { get; set; } = default!;
+    }
+}
