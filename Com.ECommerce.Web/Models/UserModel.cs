7f4cc9c8c7565955ca4ea5356dad75a4c1e862ec
Binod sapota <binodsapkota96@gmail.com>
2023-07-05 18:14:07 +0545

    added model for user

---
+++ Com.ECommerce.Web/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Com.ECommerce.Web.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
